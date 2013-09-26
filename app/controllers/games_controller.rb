2221bcdf5cf3e27d3fb758b6aecd95df9149a80a
Ravindra <rkn047@gmail.com>
2013-09-26 11:11:52 +0530

    game bug solved

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,33 +1,38 @@
 class GamesController < ApplicationController
+
   skip_before_filter :verify_authenticity_token
 
   def new_game
     @game = HANGMAN::Hangman.new('Support'.downcase)
     session[:game] = @game
     #puts @game.word
+    current_user.game.played += 1
+    session[:game_flag] = true
     redirect_to game_path
+
   end
 
   def new
     #@game = HANGMAN::Hangman.new('Support'.downcase)
     @game = session[:game]
+    if session[:game_flag]
+      if @game.counter == @game.word.length
+        @result = "You Won"
+        @result_word = "Game Word ::- " + @game.word
+        current_user.game.win += 1
+        game = current_user.game
+        game.save
+        session[:game_flag] = false
+      end
 
-    if @game.counter == @game.word.length
-      @result = "You Won"
-      @result_word = "Game Word ::- " + @game.word
-      current_user.game.played += 1
-      current_user.game.win += 1
-      game = current_user.game
-      game.save
-    end
-
-    if @game.missed_counter == 6
-      @result = "You lost"
-      @result_word = "Game Word ::- " + @game.word
-      current_user.game.played += 1
-      current_user.game.lose += 1
-      game = current_user.game
-      game.save
+      if @game.missed_counter == 6
+        @result = "You lost"
+        @result_word = "Game Word ::- " + @game.word
+        current_user.game.lose += 1
+        game = current_user.game
+        game.save
+        session[:game_flag] = false
+      end
     end
     #puts @game.word
   end
