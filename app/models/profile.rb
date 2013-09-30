ee3c5680fb694f29d9c97e1a80db01872b2c81c7
kundan <kundan@iternia.com>
2013-09-30 12:16:49 +0530

    add missing image

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
   validates :first_name, :last_name, presence: true, format: {with: /\A[a-zA-Z\s .]+\z/, message: "No special characters allowed except space & dot"}
   validates :gender, presence: true
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates :avatar, :attachment_presence => true
+  #validates :avatar, :attachment_presence => true
 =begin
   validates_attachment :avatar, :presence => true,
                        :content_type => { :content_type => "image/jpg" },
